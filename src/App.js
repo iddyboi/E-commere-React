cf7627410a9f91c0dd59e515ef950c41fe33e187
Idris <idrist2013@gmail.com>
2020-03-29 15:28:37 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./page/homepage/Homepage.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
+
+function MenuItem({ title, imageUrl, size, history, linkUrl, match }) {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtile">Shop Now</span>
+      </div>
+    </div>
+  );
+}
+export default withRouter(MenuItem);
